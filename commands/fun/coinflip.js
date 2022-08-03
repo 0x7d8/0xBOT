63d5287ec38d12d5a7327beec4de84f49372bf14
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-08-03 16:36:17 +0200

    [0.9.9] #0 - ADD ABILITY TO FLIP UP TO 1000 COINS

--- commands/fun/coinflip.js
+++ commands/fun/coinflip.js
@@ -6,7 +6,11 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('coinflip')
     	.setDMPermission(false)
-        .setDescription('WIRF EINE MÜNZE'),
+        .setDescription('WIRF EINE MÜNZE')
+        .addIntegerOption(option =>
+            option.setName('anzahl')
+                .setDescription('DIE ANZAHL')
+                .setRequired(false)),
     async execute(interaction) {
         // Count to Global Commands
         addcmd('t-all', 1)
@@ -16,35 +20,84 @@ module.exports = {
         addcmd('u-' + interaction.user.id, 1)
         
         // Set Variables
-        const random = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
+        var anzahl = interaction.options.getInteger("anzahl")
+        var heads = 0
+        var tails = 0
+        var tries = 0
         
         // Check Maintenance
         const { maintenance } = require('../../config.json');
         if (maintenance == 'yes' && interaction.user.id != '745619551865012274') {
             // Create Embed
-            var mterr = new EmbedBuilder()
+            var err = new EmbedBuilder()
         		.setTitle('» FEHLER')
         		.setDescription('» Der Bot ist aktuell unter Wartungsarbeiten!')
         		.setFooter({ text: '» ' + version });
             
-            return interaction.reply({ embeds: [mterr.toJSON()], ephemeral: true })
+            return interaction.reply({ embeds: [err.toJSON()], ephemeral: true })
+        }
+
+        // Check if Number is empty
+        if (anzahl == null) {
+            var anzahl = 1
         }
         
-        // Translate to Word
-        if (random == '1') {
-            var coin = 'KOPF'
-        } else {
-            var coin = 'ZAHL'
+        // Check if Number is negative
+        if (anzahl < 1) {
+            // Create Embed
+            var err = new EmbedBuilder()
+        		.setTitle('» FEHLER')
+        		.setDescription('» Du musst schon mindestens eine Münze werfen!')
+        		.setFooter({ text: '» ' + version });
+            
+            return interaction.reply({ embeds: [err.toJSON()], ephemeral: true })
         }
+
+        // Check if Number is too Big
+        if (anzahl > 1000) {
+            // Create Embed
+            var err = new EmbedBuilder()
+        		.setTitle('» FEHLER')
+        		.setDescription('» Du darfst nicht mehr als **1000** Münzen werfen!')
+        		.setFooter({ text: '» ' + version });
+            
+            return interaction.reply({ embeds: [err.toJSON()], ephemeral: true })
+        }
+
+        // Loop
+        loop:
+        while (anzahl != tries) {
+            var random = Math.floor(Math.random() * (2 - 1 + 1)) + 1;
+
+            if (random == 1) { var coin = 'KOPF'; var heads = heads + 1 }
+            if (random == 2) { var coin = 'ZAHL'; var tails = tails + 1 }
+
+            var tries = tries + 1
+            continue loop;
+        }
+
+        // Add Zeros
+        if (heads < 10) { var heads = '0' + heads}
+        if (heads < 100) { var heads = '0' + heads}
+        if (tails < 10) { var tails = '0' + tails}
+        if (tails < 100) { var tails = '0' + tails}
         
         // Create Embed
-        const message = new EmbedBuilder()
-        	.setTitle('» COINFLIP')
-  			.setDescription('» Die Münze ist auf **' + coin + '** gelandet!')
-        	.setFooter({ text: '» ' + version });
+        if (anzahl == 1) {
+            var message = new EmbedBuilder()
+        	    .setTitle('» COINFLIP')
+  			    .setDescription('» Die Münze ist auf **' + coin + '** gelandet!')
+        	    .setFooter({ text: '» ' + version });
+        } else {
+            var message = new EmbedBuilder()
+        	    .setTitle('» COINFLIP')
+  			    .setDescription('» KÖPFE\n`' + heads + '`\n» ZAHLEN\n`' + tails + '`')
+        	    .setFooter({ text: '» ' + version });
+        }
+
         
         // Send Message
-        console.log('[0xBOT] [i] [' + interaction.user.id + ' @ ' + interaction.guild.id + '] COINFLIP : ' + coin)
+        console.log('[0xBOT] [i] [' + interaction.user.id + ' @ ' + interaction.guild.id + '] COINFLIP : H[' + heads + '] : T[' + tails + ']')
         return interaction.reply({ embeds: [message.toJSON()] })
     },
 };
