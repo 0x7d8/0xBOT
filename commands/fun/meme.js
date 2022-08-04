6c29b5126737517b05175b0dd40ea83f03240163
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-08-04 12:02:02 +0200

    [1.1.0] #0 - ADD MEME COMMAND

---
+++ commands/fun/meme.js
@@ -0,0 +1,45 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('@discordjs/builders');
+const { version } = require('../../config.json');
+const fetch = require("node-fetch");
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('meme')
+    	.setDMPermission(false)
+        .setDescription('BEKOMME EIN MEME'),
+    async execute(interaction) {
+        // Count to Global Commands
+        addcmd('t-all', 1)
+        
+        // Count Guild Commands and User
+        addcmd('g-' + interaction.guild.id, 1)
+        addcmd('u-' + interaction.user.id, 1)
+
+        // Set Variables
+        const url = await fetch("https://www.reddit.com/r/memes/random/.json");
+        const random = await url.json();
+        
+        // Check Maintenance
+        const { maintenance } = require('../../config.json');
+        if (maintenance == 'yes' && interaction.user.id != '745619551865012274') {
+            // Create Embed
+            var err = new EmbedBuilder()
+        		.setTitle('» FEHLER')
+        		.setDescription('» Der Bot ist aktuell unter Wartungsarbeiten!')
+        		.setFooter({ text: '» ' + version });
+            
+            return interaction.reply({ embeds: [err.toJSON()], ephemeral: true })
+        }
+        
+        // Create Embed
+        var message = new EmbedBuilder()
+            .setTitle(`» Zufälliges Meme **| ${random[0].data.children[0].data.title}**`)
+            .setImage(random[0].data.children[0].data.url)
+        	.setFooter({ text: '» ' + version });
+        
+        // Send Message
+        console.log('[0xBOT] [i] [' + interaction.user.id + ' @ ' + interaction.guild.id + '] MEME')
+        return interaction.reply({ embeds: [message.toJSON()] })
+    },
+};
