e2019a199c9792e906649d766241dc11962b738e
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-04 09:43:49 +0200

    [1.5.2] #0 - ADD MORE TIMESTAMPS

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
 	event: 'ready',
 	once: true,
 	execute(client) {
-		console.log(`[0xBOT] [!] STARTED AND LOGGED IN AS ${client.user.tag}!\n\n[0xBOT] [i] STARTED LOGGING OF COMMANDS AND ERRORS`);
+		console.log(`[0xBOT] [!] [${new Date().toLocaleTimeString()}] STARTED AND LOGGED IN AS ${client.user.tag}!\n\n[0xBOT] [i] [${new Date().toLocaleTimeString()}] STARTED LOGGING OF COMMANDS AND ERRORS`);
 	},
 };
