516d5d77d9925e937249ee2c0769190037e965a0
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-04 11:46:31 +0200

    [1.5.5] #0 - FIX ACTION COMMAND

--- commands/fun/action.js
+++ commands/fun/action.js
@@ -48,40 +48,38 @@ module.exports = {
         cmds.add('u-' + interaction.user.id.replace(/\D/g, ''), 1)
         
         // Set Variables
-        const userid = interaction.options.getUser("user")
-        const user = '<@' + userid + '>'
-        const sender = interaction.user.id.replace(/\D/g, '')
+        const user = interaction.options.getUser("user")
         const event = interaction.options.getString("action")
 
         // Create Embeds
         const box = new EmbedBuilder()
         	.setTitle('» ACTION!')
-  			.setDescription("» **" + sender + "** hat " + user + " Geschlagen! AUA.")
+  			.setDescription("» **<@" + sender + ">** hat <@" + user + "> Geschlagen! AUA.")
   			.setImage("https://media2.giphy.com/media/qyjexFwQwJp9yUvMxq/giphy.gif?cid=ecf05e479xhsqd2p8ap5zmeqbog4w7dn6kykqanap5j4zklq&rid=giphy.gif&ct=g")
         	.setFooter({ text: '» ' + version });
         const kill = new EmbedBuilder()
         	.setTitle('» ACTION!')
-  			.setDescription("» **" + sender + "** hat " + user + " Getötet! RIP.")
+  			.setDescription("» **<@" + sender + ">** hat <@" + user + "> Getötet! RIP.")
   			.setImage("https://media1.giphy.com/media/yNFjQR6zKOGmk/giphy.gif?cid=ecf05e47tyf8463zbs3431j0spus4vugtaq22m4occdccspm&rid=giphy.gif&ct=g")
         	.setFooter({ text: '» ' + version });
         const stare = new EmbedBuilder()
         	.setTitle('» ACTION!')
-  			.setDescription("» **" + sender + "** starrt " + user + " an! Gruselig.")
+  			.setDescription("» **<@" + sender + ">** starrt <@" + user + "> an! Gruselig.")
   			.setImage("https://media2.giphy.com/media/aXUU30cDBa9tVQz37V/giphy.gif?cid=ecf05e474vdm6e12euchkog2475qj5srvqa3ozinvz7xse0j&rid=giphy.gif&ct=g")
         	.setFooter({ text: '» ' + version });
         const twerk = new EmbedBuilder()
         	.setTitle('» ACTION!')
-  			.setDescription("» **" + sender + "** twerkt " + user + " an! Sehr Sus.")
+  			.setDescription("» **<@" + sender + ">** twerkt <@" + user + "> an! Sehr Sus.")
   			.setImage("https://media2.giphy.com/media/DqhwoR9RHm3EA/giphy.gif?cid=ecf05e47jxhd2do5ws18knygottsfiz0qqci5qm6x8w5ikjc&rid=giphy.gif&ct=g")
         	.setFooter({ text: '» ' + version });
         const catchp = new EmbedBuilder()
         	.setTitle('» ACTION!')
-  			.setDescription("» **" + sender + "** fängt " + user + "! WIESO?")
+  			.setDescription("» **<@" + sender + ">** fängt <@" + user + ">! WIESO?")
   			.setImage("https://media3.giphy.com/media/vsyKKf1t22nmw/giphy.gif?cid=ecf05e47kzkk3lkzs7wsxrpluelxo9pvve8x5946n7mj5rzv&rid=giphy.gif&ct=g")
         	.setFooter({ text: '» ' + version });
         const call = new EmbedBuilder()
         	.setTitle('» ACTION!')
-  			.setDescription("» **" + sender + "** ruft " + user + "! KOMM DOCH.")
+  			.setDescription("» **<@" + sender + ">** ruft <@" + user + ">! KOMM DOCH.")
   			.setImage("https://media2.giphy.com/media/NPFQpRI1KpIq9S0YKa/giphy.gif?cid=ecf05e47xfvrmgjqorm0p5hn2iz9kxjw6ngykph6bireyunn&rid=giphy.gif&ct=g")
         	.setFooter({ text: '» ' + version });
         const err = new EmbedBuilder()
