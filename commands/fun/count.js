3f813af024773d260eba921487979461755b743d
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-06 17:42:53 +0200

    [1.6.4] #0 - FIX LOG

--- commands/fun/count.js
+++ commands/fun/count.js
@@ -42,7 +42,7 @@ module.exports = {
         }
 
         // Send Message
-        console.log('[0xBOT] [i] [' + new Date().toLocaleTimeString('en-US', { hour12: false }) + '] [' + interaction.user.id.replace(/\D/g, '') + ' @ ' + interaction.guild.id + '] [CMD] BEG : ' + amount + '€')
+        console.log('[0xBOT] [i] [' + new Date().toLocaleTimeString('en-US', { hour12: false }) + '] [' + interaction.user.id.replace(/\D/g, '') + ' @ ' + interaction.guild.id + '] [CMD] COUNT')
         return interaction.reply({ embeds: [message.toJSON()], components: [button] })
     },
 };
