304679d21028c7e7bb04d9c9dd8a2b07d190dcf1
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-07 12:23:47 +0200

    [1.7.0] #0 - BE ABLE TO MAKE BUTTONS

--- buttons/fun/rps/scissor.js
+++ buttons/fun/rps/scissor.js
@@ -1,4 +1,5 @@
 const { EmbedBuilder } = require('@discordjs/builders');
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const { version } = require('../../../config.json');
 
 module.exports = {
