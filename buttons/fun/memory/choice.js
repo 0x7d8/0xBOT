009122853092222173e25b8687182bdd88193949
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-09 19:39:26 +0200

    [1.7.5] #0 - LOGGING

--- buttons/fun/memory/choice.js
+++ buttons/fun/memory/choice.js
@@ -72,7 +72,7 @@ module.exports = {
             console.log('3')
             se = true
         }
-        console.log(await eval('memorydataf' + sel + sender.toString().replace(/\D/g, '')))
+        console.log(await eval('memorydatapcn' + interaction.user.id.replace(/\D/g, '')))
 
         // Create Buttons
         let row1 = new ActionRowBuilder()
