becb56bfe4be770254f2141b2c4602d8dce8481a
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-09 15:34:48 +0200

    [1.7.5] #0 - REMOVE SNO

--- buttons/fun/memory/choice.js
+++ buttons/fun/memory/choice.js
@@ -39,7 +39,6 @@ module.exports = {
         await eval('global.memorydatad' + sel + sender.toString().replace(/\D/g, '') + ' = true')
         await eval('global.memorydatapca' + interaction.user.id.replace(/\D/g, '') + ' = memorydatapca' + interaction.user.id.replace(/\D/g, '') + ' + 1')
         let se = false
-        let sno = false
         const nums = []
         if (eval('memorydatapc' + interaction.user.id.replace(/\D/g, '') + '.includes(' + await eval('memorydataf' + sel + sender.toString().replace(/\D/g, '')) + ')') && await eval('memorydatapca' + interaction.user.id.replace(/\D/g, '')) + ' < 2') {
             await eval('global.memorydatap' + interaction.user.id.replace(/\D/g, '') + ' = memorydatap' + interaction.user.id.replace(/\D/g, '') + ' + 1')
@@ -49,20 +48,18 @@ module.exports = {
             await eval('global.memorydatapc' + interaction.user.id.replace(/\D/g, '') + ' = []')
             await eval('global.memorydatapcn' + interaction.user.id.replace(/\D/g, '') + ' = []')
             console.log('1')
-            sno = true
             se = true
         }
-        if (await eval('memorydatapca' + interaction.user.id.replace(/\D/g, '')) + ' < 2' && sno == false) {
+        if (await eval('memorydatapca' + interaction.user.id.replace(/\D/g, '')) + ' < 2') {
             if (!await eval('memorydatapc' + interaction.user.id.replace(/\D/g, '') + '.includes(' + await eval('memorydataf' + sel + sender.toString().replace(/\D/g, '')) + ')')) {
                 await eval('memorydatapc' + interaction.user.id.replace(/\D/g, '') + '.push("' + await eval('memorydataf' + sel + sender.toString().replace(/\D/g, '')) + '")')
                 await eval('memorydatapcn' + interaction.user.id.replace(/\D/g, '') + '.push("' + sel + '")')
                 await eval('global.memorydatad' + sel + sender.toString().replace(/\D/g, '') + ' = true')
                 console.log('2')
-                sno = true
                 se = false
             }
         }
-        if (await eval('memorydatapca' + interaction.user.id.replace(/\D/g, '') + ' > 1') && sno == false) {
+        if (await eval('memorydatapca' + interaction.user.id.replace(/\D/g, '') + ' > 1')) {
             nums.push(eval('memorydatapcn' + interaction.user.id.replace(/\D/g, '') + '[0]'))
             nums.push(eval('memorydatapcn' + interaction.user.id.replace(/\D/g, '') + '[1]'))
             await eval('global.memorydatapca' + interaction.user.id.replace(/\D/g, '') + ' = 0')
