f93a9cba18e464f30d1aebf62129112dd93179d6
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-10 21:04:57 +0200

    [1.8.0 #0 - MAYBE FIX LOGO

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { ShardingManager } = require('discord.js');
 const { token } = require('./config.json');
 
-const manager = new ShardingManager('./bot.js', { token: token, shards: 'auto' });
+// Show Logo
+console.log('  /$$$$$$            /$$$$$$$   /$$$$$$  /$$$$$$$$')
+console.log(' /$$$_  $$          | $$__  $$ /$$__  $$|__  $$__/')
+console.log('| $$$$\\ $$ /$$   /$$| $$  \\ $$| $$  \\ $$   | $$   ')
+console.log('| $$ $$ $$|  $$ /$$/| $$$$$$$ | $$  | $$   | $$   ')
+console.log('| $$\\ $$$$ \\  $$$$/ | $$__  $$| $$  | $$   | $$   ')
+console.log('| $$ \\ $$$  >$$  $$ | $$  \\ $$| $$  | $$   | $$   ')
+console.log('|  $$$$$$/ /$$/\\  $$| $$$$$$$/|  $$$$$$/   | $$   ')
+console.log(' \\______/ |__/  \\__/|_______/  \\______/    |__/   ')
 
+const manager = new ShardingManager('./bot.js', { token: token, shards: 'auto' });
 manager.on('shardCreate', shard => console.log('[0xBOT] [i] [' + new Date().toLocaleTimeString('en-US', { hour12: false }) + '] [INF] LAUNCHED SHARD #' + shard.id));
 manager.spawn();
