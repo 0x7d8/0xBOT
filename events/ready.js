afe87b4ceefc8ed685aae7303617fd28c4fab506
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-10 21:30:55 +0200

    [1.8.0 #0 - MAKE IT SATISFYING

--- events/ready.js
+++ events/ready.js
@@ -5,8 +5,8 @@ module.exports = {
 	event: 'ready',
 	once: true,
 	execute(client) {
-		console.log(`[0xBOT] [i] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [END] $$$$$ LOADED 0xBOT ${version}`)
 		console.log(`[0xBOT] [i] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [INF] STARTED AND LOGGED IN AS ${client.user.tag}!`)
+		console.log(`[0xBOT] [i] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [END] $$$$$ LOADED 0xBOT ${version}`)
 		console.log(' ')
 		console.log(`[0xBOT] [i] [${new Date().toLocaleTimeString('en-US', { hour12: false })}] [STA] $$$$$ STARTED LOGGING OF COMMANDS AND ERRORS`)
 		console.log(' ')
