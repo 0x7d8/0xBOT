1248bd0b6939655c067dad4b9e78dcb121360f3f
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-11 10:02:41 +0200

    [1.8.1 #0 - CHANGES SOME LOGGING

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ console.log(' /$$$_  $$          | $$__  $$ /$$__  $$|__  $$__/')
 console.log('| $$$$\\ $$ /$$   /$$| $$  \\ $$| $$  \\ $$   | $$   ')
 console.log('| $$ $$ $$|  $$ /$$/| $$$$$$$ | $$  | $$   | $$   ')
 console.log('| $$\\ $$$$ \\  $$$$/ | $$__  $$| $$  | $$   | $$   ')
-console.log('| $$ \\ $$$  >$$  $$ | $$  \\ $$| $$  | $$   | $$   ')
+console.log('| $$ \\ $$$  \\$$  $$ | $$  \\ $$| $$  | $$   | $$   ')
 console.log('|  $$$$$$/ /$$/\\  $$| $$$$$$$/|  $$$$$$/   | $$   ')
 console.log(' \\______/ |__/  \\__/|_______/  \\______/    |__/   ')
 console.log(' ')
@@ -16,5 +16,5 @@ console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$')
 console.log(' ')
 
 const manager = new ShardingManager('./bot.js', { token: token, shards: 'auto' });
-manager.on('shardCreate', shard => console.log('[0xBOT] [i] [' + new Date().toLocaleTimeString('en-US', { hour12: false }) + '] [INF] LAUNCHED SHARD #' + shard.id));
+manager.on('shardCreate', shard => console.log('[0xBOT] [i] [' + new Date().toLocaleTimeString('en-US', { hour12: false }) + '] [STA] $$$$$ LAUNCHED SHARD #' + shard.id));
 manager.spawn();
