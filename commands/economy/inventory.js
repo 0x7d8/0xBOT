f26a4d15d29db95f263e3adf2e14e2c0f9f03c80
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-20 20:10:05 +0200

    [2.0.0] #0 - ADD CARS TO INVENTORY

--- commands/economy/inventory.js
+++ commands/economy/inventory.js
@@ -24,18 +24,38 @@ module.exports = {
         // Set Variables
         const user = interaction.options.getUser("user")
         
-        let nbombs, mbombs, hbombs, cbombs
+        let nbombs, mbombs, hbombs, cbombs, carname
 
         if (user == null) {
             nbombs = await item.get(interaction.user.id + '-NBOMBS-' + interaction.guild.id, 'amount')
             mbombs = await item.get(interaction.user.id + '-MBOMBS-' + interaction.guild.id, 'amount')
             hbombs = await item.get(interaction.user.id + '-HBOMBS-' + interaction.guild.id, 'amount')
             cbombs = await item.get(interaction.user.id + '-CBOMBS-' + interaction.guild.id, 'amount')
+
+            const car = await item.get(interaction.user.id + '-CAR', 'value')
+            carname = 'NONE'
+            if (lang.toString() == 'de') {
+                carname = 'KEINS'
+            }
+            if (car == 'jeep') { carname = '2016 JEEP PATRIOT SPORT' }
+            if (car == 'kia') { carname = '2022 KIA SORENTO' }
+            if (car == 'tesla') { carname = 'TESLA MODEL Y' }
+            if (car == 'porsche') { carname = '2019 PORSCHE 911 GT2RS' }
         } else {
             nbombs = await item.get(user.id + '-NBOMBS-' + interaction.guild.id, 'amount')
             mbombs = await item.get(user.id + '-MBOMBS-' + interaction.guild.id, 'amount')
             hbombs = await item.get(user.id + '-HBOMBS-' + interaction.guild.id, 'amount')
             cbombs = await item.get(user.id + '-CBOMBS-' + interaction.guild.id, 'amount')
+
+            const car = await item.get(user.id + '-CAR', 'value')
+            carname = 'NONE'
+            if (lang.toString() == 'de') {
+                carname = 'KEINS'
+            }
+            if (car == 'jeep') { carname = '2016 JEEP PATRIOT SPORT' }
+            if (car == 'kia') { carname = '2022 KIA SORENTO' }
+            if (car == 'tesla') { carname = 'TESLA MODEL Y' }
+            if (car == 'porsche') { carname = '2019 PORSCHE 911 GT2RS' }
         }
 
         // Get Userinfo
@@ -49,25 +69,25 @@ module.exports = {
         if (user == null) {
             message = new EmbedBuilder()
                 .setTitle('» YOUR INVENTORY')
-                .setDescription('» <:NBOMB:1021783222520127508> NORMAL BOMBS\n**`' + nbombs + '/10`**\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBS\n**`' + mbombs + '/10`**\n\n» <:HBOMB:1021783351947952158> HYPER BOMBS\n**`' + hbombs + '/10`**\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBS\n**`' + cbombs + '/10`**')
+                .setDescription('» <:NBOMB:1021783222520127508> NORMAL BOMBS\n**`' + nbombs + '/10`**\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBS\n**`' + mbombs + '/10`**\n\n» <:HBOMB:1021783351947952158> HYPER BOMBS\n**`' + hbombs + '/10`**\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBS\n**`' + cbombs + '/10`**\n\n» <:CAR:1021844412998877294> CAR\n**`' + carname + '`**')
                 .setFooter({ text: '» ' + vote + ' » ' + version });
 
             if (lang.toString() == "de") {
                 message = new EmbedBuilder()
                     .setTitle('» DEIN INVENTAR')
-                    .setDescription('» <:NBOMB:1021783222520127508> NORMALE BOMBEN\n**`' + nbombs + '/10`**\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBEN\n**`' + mbombs + '/10`**\n\n» <:HBOMB:1021783351947952158> HYPER BOMBEN\n**`' + hbombs + '/10`**\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBEN\n**`' + cbombs + '/10`**')
+                    .setDescription('» <:NBOMB:1021783222520127508> NORMALE BOMBEN\n**`' + nbombs + '/10`**\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBEN\n**`' + mbombs + '/10`**\n\n» <:HBOMB:1021783351947952158> HYPER BOMBEN\n**`' + hbombs + '/10`**\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBEN\n**`' + cbombs + '/10`**\n\n» <:CAR:1021844412998877294> AUTO\n**`' + carname + '`**')
                     .setFooter({ text: '» ' + vote + ' » ' + version });
             }
         } else {
             message = new EmbedBuilder()
                 .setTitle('» THE INVENTOR OF ' + username.username.toUpperCase() + '#' + username.discriminator)
-                .setDescription('» <:NBOMB:1021783222520127508> NORMAL BOMBS\n**`' + nbombs + '/10`**\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBS\n**`' + mbombs + '/10`**\n\n» <:HBOMB:1021783351947952158> HYPER BOMBS\n**`' + hbombs + '/10`**\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBS\n**`' + cbombs + '/10`**')
+                .setDescription('» <:NBOMB:1021783222520127508> NORMAL BOMBS\n**`' + nbombs + '/10`**\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBS\n**`' + mbombs + '/10`**\n\n» <:HBOMB:1021783351947952158> HYPER BOMBS\n**`' + hbombs + '/10`**\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBS\n**`' + cbombs + '/10`**\n\n» <:CAR:1021844412998877294> CAR\n**`' + carname + '`**')
                 .setFooter({ text: '» ' + vote + ' » ' + version });
 
             if (lang.toString() == "de") {
                 message = new EmbedBuilder()
                     .setTitle('» DAS INVENTAR VON ' + username.username.toUpperCase() + '#' + username.discriminator)
-                    .setDescription('» <:NBOMB:1021783222520127508> NORMALE BOMBEN\n**`' + nbombs + '/10`**\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBEN\n**`' + mbombs + '/10`**\n\n» <:HBOMB:1021783351947952158> HYPER BOMBEN\n**`' + hbombs + '/10`**\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBEN\n**`' + cbombs + '/10`**')
+                    .setDescription('» <:NBOMB:1021783222520127508> NORMALE BOMBEN\n**`' + nbombs + '/10`**\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBEN\n**`' + mbombs + '/10`**\n\n» <:HBOMB:1021783351947952158> HYPER BOMBEN\n**`' + hbombs + '/10`**\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBEN\n**`' + cbombs + '/10`**\n\n» <:CAR:1021844412998877294> AUTO\n**`' + carname + '`**')
                     .setFooter({ text: '» ' + vote + ' » ' + version });
             }
         }
