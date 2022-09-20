59fd385b86774151738bfe81166fc81785eee36f
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-20 16:19:03 +0200

    [2.0.0] #0 - FIX TYPO

--- commands/economy/inventory.js
+++ commands/economy/inventory.js
@@ -60,13 +60,13 @@ module.exports = {
             }
         } else {
             message = new EmbedBuilder()
-                .setTitle('» THE STOCKS OF ' + username.username.toUpperCase() + '#' + username.discriminator)
+                .setTitle('» THE INVENTOR OF ' + username.username.toUpperCase() + '#' + username.discriminator)
                 .setDescription('» <:NBOMB:1021783222520127508> NORMAL BOMBS\n`' + nbombs + '`\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBS\n`' + mbombs + '`\n\n» <:HBOMB:1021783351947952158> HYPER BOMBS\n`' + hbombs + '`\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBS\n`' + cbombs + '`')
                 .setFooter({ text: '» ' + vote + ' » ' + version });
 
             if (lang.toString() == "de") {
                 message = new EmbedBuilder()
-                    .setTitle('» DIE AKTIEN VON ' + username.username.toUpperCase() + '#' + username.discriminator)
+                    .setTitle('» DAS INVENTAR VON ' + username.username.toUpperCase() + '#' + username.discriminator)
                     .setDescription('» <:NBOMB:1021783222520127508> NORMALE BOMBEN\n`' + nbombs + '`\n\n» <:MBOMB:1021783295211601940> MEDIUM BOMBEN\n`' + mbombs + '`\n\n» <:HBOMB:1021783351947952158> HYPER BOMBEN\n`' + hbombs + '`\n\n» <:CBOMB:1021783405161091162> CRAZY BOMBEN\n`' + cbombs + '`')
                     .setFooter({ text: '» ' + vote + ' » ' + version });
             }
