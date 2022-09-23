9d5b4f03bd7b86bfa185ccdb6c19c23034d576a2
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-23 18:03:33 +0200

    [2.0.0] #0 - FIX TYPO

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ if (dodshbr == "yes") {(async ()=>{
                     category: "<center>UPDATE INFO</center>",
                     title: "<center>INFORMATION</center>",
                     description: `<center>This Update added the following Features: <br/><br/>Business Commands<br/>Cars<br/>Items<br/>Inventories<br/>Memory Rewrite<br/>Numerous Fixes</center>`,
-                    footer: "<center>VERSION 2.0.0-1</center>",
+                    footer: "<center>VERSION 2.0.0</center>",
                 },
 
                 feeds: {
