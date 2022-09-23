67bc196b137c3ccfb21c7475fb538a78db179a16
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-09-23 19:26:39 +0200

    [2.0.0] #0 - MAKE VOTES COMMAND NON EPHEMERAL

--- commands/misc/votes.js
+++ commands/misc/votes.js
@@ -77,6 +77,6 @@ module.exports = {
         }
 
         // Send Message
-        return interaction.reply({ embeds: [message.toJSON()], ephemeral: true })
+        return interaction.reply({ embeds: [message.toJSON()] })
     },
 };
