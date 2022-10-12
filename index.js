ee3ae2b91dfd61687bc678c2ad25c0339be49e0b
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-10-12 12:10:11 +0200

    [2.5.0] #0 - EDIT CHANGELOG

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ if (config.web.dashboard) {(async ()=>{
                 information: {
                     category: "<center>UPDATE INFO</center>",
                     title: "<center>INFORMATION</center>",
-                    description: `<center>This Update added the following Features: <br/><br/>Use Postgres instead of Mongo<br/>Massive Code Cleanup<br/>Use Numeric instead of Int</center>`,
+                    description: `<center>This Update added the following Features: <br/><br/>Use Postgres instead of Mongo<br/>Massive Code Cleanup<br/>Use Numeric instead of Int<br/>Add Baltop Command</center>`,
                     footer: "<center>VERSION 2.5.0</center>",
                 },
 
