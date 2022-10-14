6f4f50dc2735e78936c026827b77c8be9a7b60f1
Robert Jansen <63073556+rotvproHD@users.noreply.github.com>
2022-10-14 22:09:02 +0200

    [2.5.1] #0 - FIX TYPO

--- functions/economy/money.js
+++ functions/economy/money.js
@@ -1,5 +1,5 @@
 // Connect to Database
-const config = require('../config.json')
+const config = require('../../config.json')
 const pgP = require('pg').Pool
 const db = new pgP({
     host: config.database.oxbot.host,
